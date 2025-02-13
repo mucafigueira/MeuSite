ad11d0ec4cb9c3a2e20876ce24af2ca31d0d7294
Aguinaldo Muca Figueira <aguinaldomuca@gmail.com>
2025-02-13 19:54:17 +0000

    delete javascript

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-Document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
-    });
-});
+
