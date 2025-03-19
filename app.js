10c0d84d6b1f5150e69c7f08fd0c9528b4e6b695
Aguinaldo Muca Figueira <aguinaldomuca@gmail.com>
2025-03-19 14:40:40 +0000

    Renovação template of page

--- app.js
+++ app.js
@@ -10,3 +10,27 @@ btnMenu.addEventListener('click', function() {
     menu.classList.toggle('activo');
 });
 
+// Configuração dos slid
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+// Remove a classe active do slide atual e adiciona exit para sair da tela
+slides[currentSlide].classList.remove('active');
+slides[currentSlide].classList.add('exit');
+
+// Atualiza o índice do slide atual
+currentSlide = (index + 1) % slides.length;
+
+// Mostra o novo slide
+slides[currentSlide].classList.remove('exit');
+slides[currentSlide].classList.add('active');
+}
+
+// Mostra o primeiro slide ao carregar
+slides[currentSlide].classList.add('active');
+
+// Troca os slides automaticamente a cada 5 segundos
+setInterval(() => showSlide(currentSlide), 5000);
+
